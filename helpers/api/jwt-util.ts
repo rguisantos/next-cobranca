250b1a861bf00a9a94e4f8eb57a2e643fbd01046
Ribamar <ribamargsantos@hotmail.com>
2024-03-24 22:39:35 -0400

    verifica token

--- helpers/api/jwt-util.ts
+++ helpers/api/jwt-util.ts
@@ -1,12 +1,21 @@
+import { cookies } from "next/headers";
+
 const jwt = require('jsonwebtoken');
 
 export function getUserJwt(req: Request) {
-    if(!req.headers.has('authorization'))
+    
+    // if(!req.headers.has('authorization'))
+    //     return null;
+
+    // const authorization = req.headers.get('authorization');
+
+    // const token = authorization?.replace('Bearer ', '');
+
+    const cookieStore = cookies();
+    if(cookieStore.getAll().filter(x=> x.name==='JWT_AUTH').length===0)
         return null;
 
-    const authorization = req.headers.get('authorization');
-
-    const token = authorization?.replace('Bearer ', '');
+    const token = cookieStore.get('JWT_AUTH')?.value;
 
     const jwtVerify = jwt.verify(token, process.env.SECRET_KEY_JWT!, {algorithms: ['HS256'] });
     return jwtVerify;
